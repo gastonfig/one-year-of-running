a9ab19fcfe7b412d304e2103accb29c47ad007f3
Gaston Figueroa <gaston@uncorkedstudios.com>
2015-11-15 16:53:24 -0800

    Adds initial layout and some typography.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  concat = require('gulp-concat'),
+  livereload = require('gulp-livereload'),
+  sass = require('gulp-sass');
 
 gulp.task('connect', function() {
   connect.server({
@@ -10,17 +13,36 @@ gulp.task('connect', function() {
 
 gulp.task('html', function () {
   gulp.src('*.html')
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
+/* CSS
+----------------------------------------------- */
+gulp.task('sass', function() {
+  gulp.src([
+    'bower_components/normalize-css/normalize.css',
+    'scss/main.scss'
+    ])
+      .pipe(sass({errLogToConsole: true}))
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest('css/'))
+      .pipe(livereload());
+});
+
+/* JS
+----------------------------------------------- */
 gulp.task('js', function () {
   gulp.src('js/*.js')
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 gulp.task('watch', function () {
+  livereload.listen();
   gulp.watch(['*.html'], ['html']);
   gulp.watch(['js/*.js'], ['js']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch'], function() {
+
+});
