77e7e7eb9091d62d9e1f28dd5eea34bbdf1c9608
Gaston Figueroa <gaston@uncorkedstudios.com>
2016-01-12 07:23:02 -0800

    Experiments with charts and layout. WIP.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
   connect = require('gulp-connect'),
   concat = require('gulp-concat'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
   connect.server({
@@ -34,18 +35,27 @@ gulp.task('sass', function() {
 gulp.task('concat-js', function() {
   gulp.src([
     'bower_components/d3/d3.min.js',
-    'bower_components/angular/angular.js',
+    'bower_components/angular/angular.min.js',
     'src/js/app.js',
     ])
     .pipe(concat('scripts.min.js'))
-    .pipe(gulp.dest('js/'))
+    .pipe(gulp.dest('js'))
+    // .pipe(uglify({ mangle: false }))
+    // .pipe(gulp.dest('js'))
     .pipe(livereload());
 });
+
+// gulp.task('compress', function() {
+//   return gulp.src('js/scripts.min.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('js'))
+//     .pipe(livereload());
+// });
 
 gulp.task('watch', function () {
   livereload.listen();
   gulp.watch(['*.html'], ['html']);
-  gulp.watch(['src/js/**/*.js', 'js/**/*.js'], ['concat-js']);
+  gulp.watch(['src/js/**/*.js'], ['concat-js']);
   gulp.watch('scss/**/*.scss', ['sass']);
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -92,32 +92,58 @@ runsApp.directive('linearChart', function($window) {
 	       	scope.$watch('data', function(newData, oldData) {
 	       		if(newData) {
 	            var data = scope[attrs.chartData],
-									d3 = $window.d3,
-									rawSvg=elem.find('svg'),
-									vis = d3.select(rawSvg[0]),
-									w = 600,
-									h = 600,
-									margin = 80,
-									TWO_PI = 2 * Math.PI,
-							    angle = TWO_PI / (data.length - 1),
-							    radius = 1.4,
-									maxDistance = d3.max(data, function(d, i) { return +d.distance;}),
-									x = d3.scale.linear().domain([0, maxDistance]).range([0 + margin, (w) - margin]),
-									y = d3.scale.linear().domain([0, maxDistance]).range([0 + margin, (h) - margin]);
+						d3 = $window.d3,
+						rawSvg=elem.find('svg'),
+						vis = d3.select(rawSvg[0]),
+						h = 800,
+						w = h * 0.69, // graph's ratio is long
+						margin = 10,
+						TWO_PI = 2 * Math.PI,
+					    angle = TWO_PI / data.length,
+					    radius = 1,
+						maxDistance = d3.max(data, function(d, i) { return + d.distance;}),
+						x = d3.scale.linear().domain([0, maxDistance]).range([0 + margin, (h) - margin]),
+						y = d3.scale.linear().domain([0, maxDistance]).range([0 + margin, (h) - margin]),
 
+						//
+						ringNum = 9,
+						vertPlacement = 2.3;  // hight given chart's shape
 
-					    vis
-					        // .attr("width", w)
-					        // .attr("height", h)
-					        .attr('width', '100%')
-					        .attr('height', '100%')
-					        .attr('viewBox','0 0 '+ w +' '+ h)
-					        .attr('preserveAspectRatio','xMinYMin')
+						// RINGS
+						var rings = vis.append('svg:g');
+						for(var i = ringNum + 1; i > 2; i--) {
+							rings.append('svg:circle')
+							.attr("class", "step")
+							.attr('r', function() {
+								return 0;
+							})
+							.attr('cx', function() {
+								return w/2;
+							})
+							.attr('cy', function() {
+								return h/vertPlacement;
+							})
+							.attr('opacity', 0)
+						  
+						  .transition()
+								// .attr('opacity', 1)
+								.attr('r', function() {
+									return Math.round(radius * x(1609.34 * (i-1)) / 2); // In meters
+								})
+								.duration(1000);
+						};
+
+					    vis.attr("width", w)
+					        .attr("height", h)
+					        // .attr('width', '100%')
+					        // .attr('height', '100%')
+					        // .attr('viewBox','0 0 '+ w +' '+ h)
+					        // .attr('preserveAspectRatio','xMinYMin')
 
 					    var g = vis.append('svg:g')
-					        .attr("transform", "translate(" + w/2 +", " + h/2.5 + ")"); // 1/3 hight given chart's shape
+					        .attr("transform", "translate(" + w/2 +", " + h/vertPlacement + ")");
 
-					    var line = d3.svg.line()
+					    var line = d3.svg.line()					    	
 					        // .interpolate("cardinal")
 					        .x(function(d,i) { 
 					            return radius * x(d.distance) * Math.sin(angle * i) / 2;
@@ -126,51 +152,278 @@ runsApp.directive('linearChart', function($window) {
 					            return radius * y(d.distance) * Math.cos(angle * i) /2 * -1;
 					        });
 
-						    g.append("svg:path")
-						     .attr("d", line(data))
-						     .attr('stroke', '#7545E6')
-						     .attr('stroke-width', 2)
-							    .attr('fill', 'transparent')
-							    // .attr('fill', 'rgba(11,197,230,.03)')
+					    var lineLoad = d3.svg.line()
+					        // .interpolate("cardinal")
+					        .x(0)
+					        .y(0);
 
-						// Circles
-						var circles = vis.append('svg:g').selectAll('.circle')
+					    // MAIN PATH
+					    g.append("svg:path")
+								.attr("d", lineLoad(data))
+								.attr('stroke', '#7545E6')
+								.attr('stroke-width', 1)
+								.attr('fill', 'transparent')
+								.attr('class', 'path')
+								.transition()
+									.attr("d", line(data))
+									.duration(1000)
+									.delay(400)
+									.ease('elastic', 1, 1.25);
+
+						// Lines
+						var lines = vis.append('svg:g')
+							  .attr("transform", "translate(" + w/2 +", " + h/vertPlacement + ")")
+								.selectAll('.line')
+						    .data(data)
+						    .enter().append('line')
+							    .attr("class", "line")
+							    .attr('x1', function(d, i) { 
+							    	return radius * x(1609.34 * 2) * Math.sin(angle * i) / 2;
+							    })
+							    .attr('y1', function(d, i) { 
+							    	return radius * y(1609.34 * 2) * Math.cos(angle * i) / 2 * -1;
+							    })
+							    .attr('x2', function(d, i) {
+							    	return radius * x(1609.34 * 2) * Math.sin(angle * i) / 2;
+							    })
+							    .attr('y2', function(d, i) { 
+							    	return radius * y(1609.34 * 2) * Math.cos(angle * i) / 2 * -1;
+							    })
+								.attr('opacity', 0)
+							  .transition()
+									.attr('opacity', .7)
+									.attr('x2', function(d, i) {
+										return radius * y(1609.34 * ringNum) * Math.sin(angle * i) / 2;
+									})
+									.attr('y2', function(d, i) { 
+										return radius * y(1609.34 * ringNum) * Math.cos(angle * i) / 2 * -1;
+									})
+									.duration(1000);
+
+						// DOTS
+						var circles = vis
+								.append('svg:g')
+						    .attr("transform", "translate(" + w/2 +", " + h/vertPlacement + ")")
+								.selectAll('.circle')
 						    .data(data)
 						  .enter().append('svg:circle')
 						    .attr("class", "circle")
-						    .attr("transform", "translate(" + w/2 +", " + h/2.5 + ")") // 1/3 hight given chart's shape
-						    .attr('r', 4)
-						    .attr('cx', function(d, i) {
-						                  return radius * x(d.distance) * Math.sin(angle * i) / 2;
-						                })
-						    .attr('cy', function(d, i) {
-						                  return radius * y(d.distance) * Math.cos(angle * i) / 2 * -1;
-						                })
-						    // .attr('fill', '#09C4E6')
-						    // .attr('fill', 'transparent')
-						    .attr('stroke', '#09C4E6')
-						    .attr('stroke-width', 2.5);
+						    .attr('r', function(d) {
+						    	// return d.max_speed * .9;
+						    	return 4;
+						    })
+								.attr('cx', 0)
+								.attr('cy', 0)	
+						    .attr('opacity', 0)
+						    // .attr('fill', 'rgba(255, 255, 255, .8)')
+						    // .attr('fill', '#7545E6')
+						    // .attr('stroke', 'rgba(255, 255, 255, .17)')
+						    // .attr('stroke', '#020722')
+						    // .attr('stroke-width', 2)
+						    .transition()
+							    // .duration(500)
+									// .delay(400)
 
-						// Rings
-						for(var i = 0; i < 6; i++) {
-							vis.append('svg:g')
-							.append('svg:circle')
-							.attr("class", "step")
-							.attr('r', function() {
-								return  radius * x((1609.34 * 1.5) * i) / 2; // In meters
-							})
-							.attr('cx', function() {
-								return w/2;
-							})
-							.attr('cy', function() {
-								return h/2.5;
-							})
-							.attr('opacity', .17)
-							.attr('stroke', '#fff')
-							.attr('fill', 'none');
-							
+						    
+						    // .transition()
+								.attr('cx', function(d, i) {
+									return radius * x(d.distance) * Math.sin(angle * i) / 2;
+								})
+							    .attr('cy', function(d, i) {
+							    	return radius * y(d.distance) * Math.cos(angle * i) / 2 * -1;
+							    })
+							    .duration(1200)
+							    // .delay(100 * i)
+								  .delay(function(d, i) {
+							    	return 10 * i;
+							    })
+							    .attr('opacity', 1)
+								  .ease('elastic', 1, 1.25);
+
+						
+	       		}
+			}, true);
+		}
+	};
+})
+
+// RUN DOTS CHART
+runsApp.directive('runsDotsChart', function($window) {
+   return {
+      restrict:'EA',
+      template:"<svg></svg>",
+       link: function(scope, elem, attrs) {
+	       	scope.$watch('data', function(newData, oldData) {
+	       		if(newData) {
+	            var data = scope[attrs.chartData],
+					d3 = $window.d3,
+					rawSvg = elem.find('svg'),
+					vis = d3.select(rawSvg[0]),
+					h = 300,
+					w = 600,
+					margin = 10,
+					TWO_PI = 2 * Math.PI,
+					angle = TWO_PI / data.length,
+					dotRadius = 5,
+					months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec'],
+
+		            nest = d3.nest().key(function(d) { return d.start_date_local.substring(5, 7) }).sortKeys(d3.ascending).entries(data),
+		            nestMiles = d3.nest().key(function(d) { return d.start_date_local.substring(5, 7) }).sortKeys(d3.ascending)
+					            .rollup(function(d) { return d3.sum(d, function(g) {return g.distance; }); })
+					            .entries(data),
+					maxMiles = d3.max(nestMiles, function(d, i) { return + d.values;}),
+					maxRunsNum = d3.max(nest, function(d, i) { return + d.values.length;}),
+					x = d3.scale.linear().domain([1, 12]).range([margin, w - margin]), // Scale to 2 months
+					y = d3.scale.linear().domain([0, maxRunsNum + 1]).range([margin, h - margin]), // Adding 1 to maxRunsNum to account for text placement
+					color = d3.scale.linear().domain([0, maxMiles]).range(['#09A3E6', '#AF09E6']);
+
+		            console.log(maxMiles)
+
+				    vis.attr("width", w).attr("height", h);
+
+
+				    var g = vis.selectAll('g')
+				    	.data(nest).enter()
+				    	.append('svg:g')
+				    	.attr('class', 'circle-container')
+				        .attr("transform", function(d, i) {
+				        	return "translate(" + x(d.key) + ", " + (h  - (dotRadius * 2) - (margin * 3)) + ")"
+				        });
+
+					g.selectAll('circle')
+					    .data(function(d) {
+					    	return d.values;
+					    }).enter()
+					    	.append('circle')
+					    	.attr('r', dotRadius)
+					    	.attr('fill', function(d, i) {
+					    		return color(nestMiles[i].values);
+					    	})
+					    	// .attr('r', function(d) {
+						    // 	console.log(d.distance)
+					    	// 	return d.distance / 1000;
+					    	// })
+					    	.attr('cy', function(d, i) {
+					    		return y(-i);
+					    	})
+					    	.attr('class', 'runs-circle')
+
+					// TEXT
+					for(var i = 0; i < 12; i++) {
+						vis.append('text')
+						.attr('class', 'runs-label')
+						.text(function() {
+							return months[i];
+						})
+						.attr("text-anchor", "middle")
+						.attr('x', function() {
+							return x(i+1);
+						})
+						.attr('y', function() {
+							return h;
+						})
+						.attr('fill', '#fff')
+					}
+	       		}
+			}, true);
+		}
+	};
+})
+
+// RUN DOTS CHART
+runsApp.directive('milesLineChart', function($window) {
+   return {
+      restrict:'EA',
+      template:"<svg></svg>",
+       link: function(scope, elem, attrs) {
+	       	scope.$watch('data', function(newData, oldData) {
+	       		if(newData) {
+	            var data = scope[attrs.chartData],
+					d3 = $window.d3,
+					rawSvg = elem.find('svg'),
+					vis = d3.select(rawSvg[0]),
+					h = 300,
+					w = 600,
+					margin = 10,
+					TWO_PI = 2 * Math.PI,
+					angle = TWO_PI / data.length,
+					rectWidth = 6,
+					months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec'],
+
+		            nest = d3.nest().key(function(d) { return d.start_date_local.substring(5, 7) }).sortKeys(d3.ascending)
+		            .rollup(function(d) { return d3.sum(d, function(g) {return g.distance; }); })
+		            .entries(data),
+					maxRunsNum = d3.max(nest, function(d, i) { return + d.values;}),
+					x = d3.scale.linear().domain([1, 12]).range([margin, w - margin]), // Scale to 12 months
+					y = d3.scale.linear().domain([0, maxRunsNum]).range([margin, h - margin - rectWidth])
+
+					color = d3.scale.linear().domain([0, maxRunsNum]).range(['#0984E6', '#E6098A']);
+
+		            // console.log(maxRunsNum)
+
+				    vis.attr("width", w).attr("height", h);
+
+				    var defs = vis.append("defs");
+
+				    var g = vis.selectAll('g')
+				    	.data(nest).enter()
+				    	.append('rect')
+				    		.attr('width', rectWidth)
+					    	.attr("y", function(d) {
+					    		// console.log( d.values);
+					    		return -y(d.values);
+					    	})
+					    	.attr("x", -rectWidth/2)
+					    	.attr("height", function(d) {
+					    		return y(d.values);
+					    	})
+					    	.attr('class', 'miles-line')
+					        .attr("transform", function(d, i) {
+					        	return "translate(" + x(d.key) + ", " + (h  - rectWidth - (margin)) + ")"
+					        })
+					        .style("fill", function(d, i) {
+					        	return "url(#the-gradient" + i + ")";
+					        });
+
+					for(var i = 0; i < 12; i++) {
+
+						// GRADIENT
+						if(nest[i]) {
+						    var gradient = defs
+							    .append("linearGradient")
+							    .attr("id", "the-gradient" + i)
+							    .attr("x1", "0%")
+							    .attr("y1", "0%")
+							    .attr("x2", "0%")
+							    .attr("y2", "100%")
+							    .attr("spreadMethod", "pad");
+
+							    gradient.append("stop")
+								    .attr("offset", "0%")
+								    .attr("stop-color", color(nest[i].values))
+								    .attr("stop-opacity", 1);
+
+							    gradient.append("stop")
+								    .attr("offset", "100%")
+								    .attr("stop-color", "#0984E6")
+								    .attr("stop-opacity", 1);
 						}
 
+						// TEXT
+						vis.append('text')
+						.attr('class', 'runs-label')
+						.text(function() {
+							return months[i];
+						})
+						.attr("text-anchor", "middle")
+						.attr('x', function() {
+							return x(i+1);
+						})
+						.attr('y', function() {
+							return h;
+						})
+						.attr('fill', '#fff')
+					}
 	       		}
 			}, true);
 		}
