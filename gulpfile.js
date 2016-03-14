757eb146f9c5d10551145da82fc584164287b3e1
Gaston Figueroa <gaston@uncorkedstudios.com>
2016-03-13 20:32:36 -0700

    Folder re-org.

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,20 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   livereload = require('gulp-livereload'),
   sass = require('gulp-sass'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  DIST_DIR = 'dist/',
+  SRC_DIR = 'src/';
 
 gulp.task('connect', function() {
   connect.server({
+    root: DIST_DIR,
     livereload: true,
     port: 8888
   });
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
+  gulp.src(DIST_DIR + '*.html')
     .pipe(livereload());
 });
 
@@ -24,11 +27,11 @@ gulp.task('html', function () {
 gulp.task('sass', function() {
   gulp.src([
     'bower_components/normalize-css/normalize.css',
-    'scss/main.scss'
+    SRC_DIR + 'scss/main.scss'
     ])
       .pipe(sass({errLogToConsole: true}))
       .pipe(concat('styles.css'))
-      .pipe(gulp.dest('css/'))
+      .pipe(gulp.dest(DIST_DIR + 'css/'))
       .pipe(livereload());
 });
 
@@ -38,28 +41,29 @@ gulp.task('concat-js', function() {
   gulp.src([
     'bower_components/d3/d3.min.js',
     'bower_components/angular/angular.min.js',
-    'src/js/app.js',
-    'src/js/components/controllers/appControllers.js',
-    'src/js/components/filters/appFilters.js',
-    'src/js/components/directives/appDirectives.js',
+    SRC_DIR + 'js/app.js',
+    SRC_DIR + 'js/components/controllers/appControllers.js',
+    SRC_DIR + 'js/components/filters/appFilters.js',
+    SRC_DIR + 'js/components/directives/appDirectives.js',
     ])
     .pipe(concat('scripts.min.js'))
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest(DIST_DIR + 'js'))
     .pipe(livereload());
 });
 
 gulp.task('compress', function() {
-  return gulp.src('js/scripts.min.js')
+  return gulp.src(DIST_DIR + 'js/scripts.min.js')
     .pipe(uglify({ mangle: false }))
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest(DIST_DIR + 'js'))
     .pipe(livereload());
 });
 
 gulp.task('watch', function () {
   livereload.listen();
-  gulp.watch(['*.html'], ['html']);
-  gulp.watch(['src/js/**/*.js'], ['concat-js']);
-  gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch([DIST_DIR + '*.html'], ['html']);
+  gulp.watch([SRC_DIR + 'js/**/*.js'], ['concat-js']);
+  gulp.watch(SRC_DIR + 'scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['connect', 'compress', 'watch']);
+// gulp.task('default', ['connect', 'compress', 'watch']);
+gulp.task('default', ['connect', 'watch']);
